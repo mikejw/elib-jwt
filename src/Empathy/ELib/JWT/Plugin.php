73458cec6b7a87383b42a1770732b2a5798be3c4
Mike Whiting <mike@gigabyte.software>
2019-08-02 16:31:15 +0100

    add plugin

---
+++ src/Empathy/ELib/JWT/Plugin.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Empathy\ELib\JWT\Plugin {
+
+    use Empathy\MVC\Config;
+
+    /**
+     * Empathy JWT Plugin
+     * @file            Empathy/MVC/Plugin/Redbean.php
+     * @description
+     * @author          Mike Whiting
+     * @license         See LICENCE
+     *
+     * (c) copyright Mike Whiting
+
+     * with this source code in the file licence.txt
+     */
+    class Plugin extends \Empathy\MVC\Plugin implements \Empathy\MVC\Plugin\PreDispatch
+    {
+        
+        public function onPreDispatch()
+        {
+            echo 'pre-dispatch...';
+        }
+    }
+}
