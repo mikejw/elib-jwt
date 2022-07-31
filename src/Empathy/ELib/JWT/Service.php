a52851b6d18d2967c65e1ff182eaa8808b00808f
Mike Whiting <mike.whiting@eqtr.com>
2022-07-31 16:51:06 +0100

    add authenticate method

--- src/Empathy/ELib/JWT/Service.php
+++ src/Empathy/ELib/JWT/Service.php
@@ -1,23 +1,56 @@
 <?php
 
 namespace Empathy\ELib\JWT;
+use Firebase\JWT\Key;
 use Firebase\JWT\JWT;
 use Empathy\ELib\Config;
 use Empathy\MVC\DI;
+use Empathy\MVC\RequestException;
+
 
 class Service
 {
+    private $secret;
+
+    public function __construct()
+    {
+        if (($this->secret = Config::get('JWT_SECRET')) === false) {
+            throw new \Exception('No secret provided. Add to "elib.yml"');
+        }
+    }
 
 	public function generate()
     {
-        if (($secret = Config::get('JWT_SECRET')) === false) {
-            throw new \Exception('No secret provided. Add to "elib.yml"');
-        }
-
         $user = DI::getContainer()->get('CurrentUser')->getUser();
         $token = array();
-        $token['id'] = $user->id;
-        $jwt = JWT::encode($token, $secret, 'HS256');
+        $token['user_id'] = $user->id;
+        $jwt = JWT::encode($token, $this->secret, 'HS256');
         return $jwt;
+    }
+
+    public function tryAuthenticate ()
+    {
+        $token = null;
+        $request_headers = apache_request_headers();
+        $auth_header = '';
+
+        if (isset($request_headers['Authorization'])) {
+            $auth_header = $request_headers['Authorization'];
+        } elseif (isset($request_headers['authorization'])) {
+            $auth_header = $request_headers['authorization'];
+        }
+        if ($auth_header) {
+            if (preg_match('#Bearer\s(\S+)#', $auth_header, $matches)) {
+                $bearer = $matches[1];
+            }
+            if ($bearer) {
+                try {
+                    $token = JWT::decode($bearer, new Key($this->secret, 'HS256')); 
+                } catch (Exception $e) {
+                    throw new RequestException('Not authenticated', RequestException::NOT_AUTHENTICATED);
+                }
+            }
+        }
+        return $token;
     }
 }
